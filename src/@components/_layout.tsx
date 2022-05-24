c726c819e2b9cfebce3a0032b7d63de73fd9ceaa
Luca Zampetti <lzampetti@gmail.com>
2022-05-24 17:17:20 +0200

    market-language

--- src/@components/_layout.tsx
+++ src/@components/_layout.tsx
@@ -3,22 +3,12 @@ import Header from '@components/header/header';
 import Main from '@components/main/main';
 import Meta from '@components/meta/meta';
 import { Page } from '@geist-ui/core';
-import Head from 'next/head';
 import React from 'react';
 
 export default function Layout({ page, children }) {
   return (
     <>
-      <Head>
-        <title>{page.meta.title}</title>
-        <meta name="description" content={page.meta.description} />
-        <meta name="keywords" content={page.meta.keywords} />
-        <meta name="robots" content={page.meta.robots} />
-        {page.alternates && page.alternates.map(alternate => (
-          <link key={`${alternate.market}-${alternate.locale}`} rel="alternate" href={alternate.href} />
-        ))}
-      </Head>
-      <Meta />
+      <Meta page={page} />
       <Page width="1024px" style={{ maxWidth: 'calc(100vw - 40px)', margin: '0 auto' }} padding={0}>
         <Page.Header>
           <Header page={page}></Header>
